e91a7b5a2b4602ff5be5f5238af6aae9f415fa56
TarasDzivik <t.dzivik@gmail.com>
2022-11-29 21:00:37 +0100

    Refactored linq query for avoid a separeted logic with getting an ID of the pokemon

--- Services/PocemonService.cs
+++ Services/PocemonService.cs
@@ -24,34 +24,32 @@ namespace Manager.PokeApi.Services
                 // TODO: make enviremental variable
                 var pokemon = await "https://pokeapi.co/api/v2/"
                     .AppendPathSegment($"pokemon/")
-                    .SetQueryParam("limit=1120")
+                    .SetQueryParam("limit=151")
                     .GetJsonAsync<Named>();
+
+                if (pokemon == null ||
+                    pokemon.Results == null)
+                {
+                    throw new Exception("No pokemon were returned");
+                }
+
                 List<NamedAPIResource> pokemonLIst = pokemon.Results;
 
                 var filteredPokemon = (from poke in pokemonLIst
                                        where poke.name.Contains(pokemonName)
                                        orderby poke.name
-                                       select poke).Take(10).ToList();
+                                       select new PokemonBase
+                                       {
+                                           PokemonName = poke.name,
+                                           PokemonId = int.Parse(poke.url.Split("/")[6])
+                                       }).Take(10).ToList();
                 
                 if (filteredPokemon.Count < 1)
                 {
                     throw new Exception($"There were no matches for {pokemonName}");
                 }
-                List<PokemonBase> pokemonBases = new();
 
-                foreach (var p in filteredPokemon)
-                {
-                    var id = p.url.Split("/")[6];
-
-                    PokemonBase pb = new PokemonBase
-                    {
-                        PokemonName = p.name,
-                        PokemonId = int.Parse(id)
-                    };
-                    pokemonBases.Add(pb);
-                }
-
-                response.PokemonList.AddRange(pokemonBases);
+                response.PokemonList.AddRange(filteredPokemon);
                 return response;
             }
             catch (Exception ex)
