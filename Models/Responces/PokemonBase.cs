f83b522a2d9f699d1097d16a009d9ef3026b56d0
TarasDzivik <t.dzivik@gmail.com>
2022-11-29 20:44:32 +0100

    Just forgot to add files to the previous commit

---
+++ Models/Responces/PokemonBase.cs
@@ -0,0 +1,18 @@
+﻿namespace Manager.PokeApi.Models.Responces
+{
+    /// <summary>
+    /// Object that contais data on pokemon search results
+    /// </summary>
+    public class PokemonBase
+    {
+        /// <summary>
+        /// Name of the pokemon being searched
+        /// </summary>
+        public string PokemonName { get; set; }
+
+        /// <summary>
+        /// Id of pokemon being searched
+        /// </summary>
+        public int PokemonId { get; set;}
+    }
+}
---
+++ Models/Responces/SearchResult.cs
@@ -0,0 +1,30 @@
+﻿#nullable enable
+using System.Text.Json.Serialization;
+
+namespace Manager.PokeApi.Models.Responces
+{
+    /// <summary>
+    /// Responce object for the return out of the API
+    /// </summary>
+    public class SearchResult<T>
+    {
+        /// <summary>
+        /// Count of items being returned.
+        /// </summary>
+        public int? Count { get; set; }
+        /// <summary>
+        /// The string containing data for the next in pagination.
+        /// </summary>
+        public string? Next { get; set; }
+
+        /// <summary>
+        /// The string containing data for the previous in pagination.
+        /// </summary>
+        public string? Previos { get; set; }
+
+        /// <summary>
+        /// The list of results objects being returned.
+        /// </summary>
+        public List<T> Results { get; set; }
+    }
+}
